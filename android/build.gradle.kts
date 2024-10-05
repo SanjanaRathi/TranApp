1a2b1cb959417a790ee136c3ba5c8c01401aa436
Sanjana Rathi <sanjana.rathi93@gmail.com>
2024-10-05 14:46:52 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,8 @@
-buildscript {
+plugins {
+// ...
+
+  // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.2" apply false
 
     repositories {
       // Make sure that you have the following two repositories
